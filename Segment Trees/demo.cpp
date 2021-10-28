54378a23e48ee1111578a9857d8ed789f301919a
NikhilAnand98 <93191123+NikhilAnand98@users.noreply.github.com>
2021-10-28 19:54:10 +0530

    Create demo.cpp

---
+++ Segment Trees/demo.cpp
@@ -0,0 +1,37 @@
+#include "segmenttree.h"
+#include <cstdio>
+
+typedef long long ll;
+
+int n, m;
+std::vector<ll> a;
+
+ll merge(ll x, ll y)
+{
+    return x + y;
+}
+
+void update(segmentTreeNode<ll, int>& node, int x)
+{
+    node.val += x; 
+}
+
+int main()
+{
+    scanf("%d%d", &n, &m);
+    
+    a.resize(n);
+    for (int i = 0; i < n; ++i) scanf("%lld", &a[i]);
+    
+    segmentTree<ll, int, merge, update> t(n, a, 0, 0);
+    
+    while (m--)
+    {
+        int opt, x, y;
+        scanf("%d%d%d", &opt, &x, &y);
+        if (opt == 1) t.modify(x, y);
+        else printf("%lld\n", t.query(x, y + 1));
+    }
+    
+    return 0;
+}
